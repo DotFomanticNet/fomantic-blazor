793785825fa675a32b13bfbf0cbee1ad17edead7
Ahmed Salah mohamed <ahmed.salah.moh@outlook.com>
2020-10-27 23:49:39 +0200

    add ExtensionWithContentBase class

---
+++ src/Fomantic.Blazor.UI/Extensions/ExtensionWithContentBase.razor.cs
@@ -0,0 +1,35 @@
+﻿///-------------------------------------------------------------------------------------------------
+/// <file>  Fomantic.Blazor.UI\Extensions\ExtentionSample.razor.cs </file>
+///
+/// <copyright file="ExtentionSample.razor.cs" company="MyCompany.com">
+/// Copyright (c) 2020 MyCompany.com. All rights reserved.
+/// </copyright>
+///
+/// <summary>   Implements the extention sample.razor class. </summary>
+///-------------------------------------------------------------------------------------------------
+
+using Fomantic.Blazor.UI;
+using Microsoft.AspNetCore.Components;
+
+namespace Fomantic.Blazor.UI
+{
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   An extention with contentbase . </summary>
+    ///
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase"/>
+    /// <seealso cref="Fomantic.Blazor.UI.IFomanticComponentExtension"/>
+    /// <seealso cref="IFomanticComponentExtension"/>
+    ///-------------------------------------------------------------------------------------------------
+
+    public partial class ExtensionWithContentBase : IFomanticComponentWithContent
+    {
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Content of the extension.   </summary>
+        ///
+        /// <value> The child content. </value>
+        ///-------------------------------------------------------------------------------------------------
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
