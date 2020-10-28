73f13bbf56ca27ec9c0a9de01caa2fb59550e59d
Ahmed Salah mohamed <ahmed.salah.moh@outlook.com>
2020-10-28 14:04:30 +0200

    change folder structure of components

--- src/Fomantic.Blazor.UI/Components/BasicComponents/SegmantComponent/SegmentDefaults.cs
+++ src/Fomantic.Blazor.UI/Components/BasicComponents/SegmantComponent/SegmentDefaults.cs
@@ -5,12 +5,6 @@
 ///-------------------------------------------------------------------------------------------------
 
 using Fomantic.Blazor.UI;
-using Microsoft.AspNetCore.Authorization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fomantic
 {
@@ -114,33 +108,5 @@ namespace Fomantic
         ///-------------------------------------------------------------------------------------------------
 
         public static bool IsPlaceholderDefault { get; set; }
-    }
-
-    ///-------------------------------------------------------------------------------------------------
-    /// <summary>   A segment. </summary>
-    ///
-    /// ### <inheritdoc/>
-    ///-------------------------------------------------------------------------------------------------
-
-    public partial class Segment
-    {
-
-        /// <summary>   Create new Segment. </summary>
-        public Segment()
-        {
-            Color = SegmentDefaults.ColorDefault;
-            ContentAlignment = SegmentDefaults.ContentAlignmentDefault;
-            ContentSpace = SegmentDefaults.ContentSpaceDefault;
-            EnterTransition = SegmentDefaults.EnterTransitionDefault;
-            EnterTransitionDuration = SegmentDefaults.EnterTransitionDurationDefault;
-            Attaching = SegmentDefaults.AttachingDefault;
-            IsCircular = SegmentDefaults.IsCircularDefault;
-            IsCompact = SegmentDefaults.IsCompactDefault;
-            IsDisabled = SegmentDefaults.IsDisabledDefault;
-            IsInverted = SegmentDefaults.IsInvertedDefault;
-            IsLoading = SegmentDefaults.IsLoadingDefault;
-            IsPlaceholder = SegmentDefaults.IsPlaceholderDefault;
-
-        }
     }
 }
