a4eaae689d69cdc3c051140a021e4363f96dfc9a
Ahmed Salah mohamed <ahmed.salah.moh@outlook.com>
2020-10-28 14:04:30 +0200

    change folder structure of components

---
+++ src/Fomantic.Blazor.UI/Components/BasicComponents/SegmantComponent/Segment.razor.cs
@@ -0,0 +1,44 @@
+﻿///-------------------------------------------------------------------------------------------------
+// file:	Elements\BasicElements\Segment.razor.cs
+//
+// summary:	Implements the segment.razor class
+///-------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fomantic
+{
+
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   A segment. </summary>
+    ///
+    /// ### <inheritdoc/>
+    ///-------------------------------------------------------------------------------------------------
+
+    public partial class Segment
+    {
+
+        /// <summary>   Create new Segment. </summary>
+        public Segment()
+        {
+            Color = SegmentDefaults.ColorDefault;
+            ContentAlignment = SegmentDefaults.ContentAlignmentDefault;
+            ContentSpace = SegmentDefaults.ContentSpaceDefault;
+            EnterTransition = SegmentDefaults.EnterTransitionDefault;
+            EnterTransitionDuration = SegmentDefaults.EnterTransitionDurationDefault;
+            Attaching = SegmentDefaults.AttachingDefault;
+            IsCircular = SegmentDefaults.IsCircularDefault;
+            IsCompact = SegmentDefaults.IsCompactDefault;
+            IsDisabled = SegmentDefaults.IsDisabledDefault;
+            IsInverted = SegmentDefaults.IsInvertedDefault;
+            IsLoading = SegmentDefaults.IsLoadingDefault;
+            IsPlaceholder = SegmentDefaults.IsPlaceholderDefault;
+
+        }
+    }
+}
